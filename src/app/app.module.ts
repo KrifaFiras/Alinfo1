0d0747ed0f6dbdc811d81c34f2a57e888d97885e
krifaFiras <krifa.firas@esprit.tn>
2022-04-23 16:16:19 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListProductComponent } from './list-product/list-product.component';
+import {FormsModule} from "@angular/forms";
+import { FormProductComponent } from './form-product/form-product.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ListProductComponent,
+    FormProductComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
